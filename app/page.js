4c5bab2fc0c6da0b47ecdd52b43355cc29ecb8e5
thoster-wq <tusharbhanushali.t@gmail.com>
2026-03-22 20:00:16 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
 	return (
 		<div className="w-full flex flex-col justify-center items-center text-center p-4 my-4">
 			<Script
-		        src="//prankhistorian.com/db09ce4b32262d0cf923996047c6cc6f/invoke.js"
+		        src="https://pl28223907.profitablecpmratenetwork.com/db09ce4b32262d0cf923996047c6cc6f/invoke.js"
 		        async
 		        data-cfasync="false"
 		        strategy="beforeInteractive"
